bdaba3c85a941453d6f158483566ab3c2397d168
niklas <sad@asd.de>
2020-06-18 20:21:26 +0200

    Testing first build
    Signed-off-by: niklas <sad@asd.de>

---
+++ main.cpp
@@ -0,0 +1,45 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 TU Dortmund -- Design Automation for Embedded Systems
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy 
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights 
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+ * copies of the Software, and to permit persons to whom the Software is 
+ * furnished to do so, subject to the following conditions: 
+ * 
+ * The above copyright notice and this permission notice shall be included in all 
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+ * SOFTWARE.
+ */
+
+
+#include <systemc.h>
+#include "src/hw/rtl/obj_dir/Vcounter.h"
+#include "verilated.h"
+
+int sc_main(int argc, char **argv) {
+
+	sc_clock clk("clk", 10, 0.5, 3, true);
+	sc_bool enable;
+
+	Vcounter *counter = new Vcounter("counter");
+	counter->clk(clk);
+	counter->reset();
+	counter->enable();
+	counter->out();
+
+	top->clk(clk);
+	while (!Verilated::gotFinish()) { sc_start(1, SC_NS); }
+	delete top;
+	exit(0);
+}
